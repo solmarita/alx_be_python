3563c992c479c6c8f3ca6375b3951ac3939c1986
Solomon Marita <officialmokua@gmail.com>
2024-06-30 15:31:41 +0300

    Adjusted weather_advice.py print string literals to match exact specified strings

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,10 +1,10 @@
 current_weather = input("What's the weather like today? (sunny/rainy/cold): ")
 
 if current_weather.casefold() == "sunny".casefold():
-    print("Wear a t-shirt and sunglasses")
+    print("Wear a t-shirt and sunglasses.")
 elif current_weather.casefold() == "rainy".casefold():
-    print("Don't forget your umbrella and raincoat.")
+    print("Don't forget your umbrella and a raincoat.")
 elif current_weather.casefold() == "cold".casefold():
     print("Make sure to wear a warm coat and a scarf.")
 else:
-    print("Sorry, I don't have recommendations for this weather")
+    print("Sorry, I don't have recommendations for this weather.")
