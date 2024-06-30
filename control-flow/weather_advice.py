b3b804447dae7af316c2bef650d713ef48287fd6
Solomon Marita <officialmokua@gmail.com>
2024-06-30 15:34:46 +0300

    Removed casefold() str method and renamed variable to match what ALX's code test is hardcorded to check

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,10 +1,10 @@
-current_weather = input("What's the weather like today? (sunny/rainy/cold): ")
+weather = input("What's the weather like today? (sunny/rainy/cold): ")
 
-if current_weather.casefold() == "sunny".casefold():
+if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
-elif current_weather.casefold() == "rainy".casefold():
+elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif current_weather.casefold() == "cold".casefold():
+elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
