03443fe1f56cace31a33abdff42129a34653d324
Solomon Marita <officialmokua@gmail.com>
2024-07-06 22:25:47 +0300

    Fixed some ALX specific wording

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -5,7 +5,7 @@ def convert_to_celsius(fahrenheit):
     return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 def convert_to_fahrenheit(celsius):
-    return (CELSIUS_TO_FAHRENHEIT_FACTOR * celsius) + 32
+    return celsius * CELSIUS_TO_FAHRENHEIT_FACTOR + 32
 
 temp = float(input("Enter the temperature to convert: "))
 unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
