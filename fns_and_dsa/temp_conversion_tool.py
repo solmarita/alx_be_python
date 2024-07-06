3654f89552ed50244ced0468b5b4bab3e0b74030
Solomon Marita <officialmokua@gmail.com>
2024-07-06 22:29:19 +0300

    Added some ALX specific wording'

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -7,11 +7,19 @@ def convert_to_celsius(fahrenheit):
 def convert_to_fahrenheit(celsius):
     return celsius * CELSIUS_TO_FAHRENHEIT_FACTOR + 32
 
-temp = float(input("Enter the temperature to convert: "))
-unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
+while True:
 
-if unit == "F":
-    print(f"{temp}°F is {convert_to_celsius(temp)}°C")
+    temp = float(input("Enter the temperature to convert: "))
+    unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
 
-if unit == "C":
-    print(f"{temp}°C is {convert_to_fahrenheit(temp)}°F")
+    if isnumeric(temp):
+        if unit == "F":
+            print(f"{temp}°F is {convert_to_celsius(temp)}°C")
+            break
+
+        if unit == "C":
+            print(f"{temp}°C is {convert_to_fahrenheit(temp)}°F")
+            break
+    else:
+        print("Invalid temperature. Please enter a numeric value.")
+
