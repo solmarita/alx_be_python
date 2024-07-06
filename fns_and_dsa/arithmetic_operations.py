5fc9bed6b3c6c1e8129cf3c9fb00ae0f917b6f1f
Solomon Marita <officialmokua@gmail.com>
2024-07-06 21:14:51 +0300

    Added fns_and_dsa directory and corrsponding files

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,21 @@
+def perform_operation(num1: float, num2: float, operation: str) -> float:
+    """Does basic arithmetic: Add, Subtract, Multiply, and Divide"""
+
+    if operation not in ("add", "subtract", "multiply", "divide"):
+        return "Enter a valid operation, ie. add, subtract, multiply, or divide"
+    
+    if operation == "divide":
+        if num2 == 0:
+            return "Can not divide by Zero!"
+        else:
+            return num1 / num2
+        
+
+    if operation == "add":
+        return num1 + num2
+    
+    if operation == "subtract":
+        return num1 - num2
+    
+    if operation == "multiply":
+        return num1 * num2
