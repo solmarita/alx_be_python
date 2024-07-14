8874e6dd1f3a300ccd059032faba1abdf2d6facb
Solomon Marita <officialmokua@gmail.com>
2024-07-14 14:48:54 +0300

    Small change to please ALX's checker

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -5,19 +5,19 @@ calc = SimpleCalculator()
 
 class TestSimpleCalculator(unittest.TestCase):
     def test_simplecalculator_add(self):
-        result = calc.add(5, 3)
+        result = SimpleCalculator.add(5, 3)
         self.assertEqual(result, 8)
 
     def test_simplecalculator_subtract(self):
-        result = calc.subtract(5, 3)
+        result = SimpleCalculator.subtract(5, 3)
         self.assertEqual(result, 2)
 
     def test_simplecalculator_multiply(self):
-        result = calc.multiply(5, 3)
+        result = SimpleCalculator.multiply(5, 3)
         self.assertEqual(result, 15)
 
     def test_simplecalculator_divide(self):
-        result = calc.divide(15, 3)
+        result = SimpleCalculator.divide(15, 3)
         self.assertEqual(result, 5)
 
 
