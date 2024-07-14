da7768a3cd397bc74475ede0f68af8b4d99b4546
Solomon Marita <officialmokua@gmail.com>
2024-07-14 14:31:34 +0300

    changed code to match what ALX checker is looking for

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,21 +1,23 @@
 import unittest
 from simple_calculator import SimpleCalculator
 
+calc = SimpleCalculator()
+
 class TestSimpleCalculator(unittest.TestCase):
     def test_simplecalculator_add(self):
-        result = SimpleCalculator.add(5, 3)
+        result = calc.add(5, 3)
         self.assertEqual(result, 8)
 
     def test_simplecalculator_subtract(self):
-        result = SimpleCalculator.subtract(5, 3)
+        result = calc.subtract(5, 3)
         self.assertEqual(result, 2)
 
     def test_simplecalculator_multiply(self):
-        result = SimpleCalculator.multiply(5, 3)
+        result = calc.multiply(5, 3)
         self.assertEqual(result, 15)
 
     def test_simplecalculator_divide(self):
-        result = SimpleCalculator.divide(15, 3)
+        result = calc.divide(15, 3)
         self.assertEqual(result, 5)
 
 
