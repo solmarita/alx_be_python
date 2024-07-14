7abce7732ed7021ba8bd4706fc40f652c06ea808
Solomon Marita <officialmokua@gmail.com>
2024-07-14 14:50:01 +0300

    Changed function names to please ALX's checker

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -4,19 +4,19 @@ from simple_calculator import SimpleCalculator
 calc = SimpleCalculator()
 
 class TestSimpleCalculator(unittest.TestCase):
-    def test_simplecalculator_add(self):
+    def test_addition(self):
         result = SimpleCalculator.add(5, 3)
         self.assertEqual(result, 8)
 
-    def test_simplecalculator_subtract(self):
+    def test_subtraction(self):
         result = SimpleCalculator.subtract(5, 3)
         self.assertEqual(result, 2)
 
-    def test_simplecalculator_multiply(self):
+    def test_multiply(self):
         result = SimpleCalculator.multiply(5, 3)
         self.assertEqual(result, 15)
 
-    def test_simplecalculator_divide(self):
+    def test_divide(self):
         result = SimpleCalculator.divide(15, 3)
         self.assertEqual(result, 5)
 
