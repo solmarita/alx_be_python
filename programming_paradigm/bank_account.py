9eff999a74abfaa781af4156101389e757740869
Solomon Marita <officialmokua@gmail.com>
2024-07-14 13:25:38 +0300

    Fixed account balance assignment

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,6 +1,6 @@
 class BankAccount:
     def __init__(self, account_balance):
-        self.account_balance = 0
+        self.account_balance = account_balance
     
     def deposit(self, amount):
         self.account_balance += amount
