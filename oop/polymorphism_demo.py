ec7d1372b6b35dfe3cc175acc5f480b189a77e8b
Solomon Marita <officialmokua@gmail.com>
2024-07-21 17:48:16 +0300

    Created Polymophism  class and implemented required attributes and magic methods

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,19 @@
+import math
+class Shape:       
+    def area(self):
+        raise NotImplementedError("Derived classes need to override this method")
+
+class Rectangle(Shape):
+    def __init__(self, length: int, width: int):
+        self.length = length
+        self.width = width
+    
+    def area (self):
+        return self.length * self.width
+
+class Circle(Shape):
+    def __init__(self, radius: int):
+        self.radius = radius
+    
+    def area(self):
+        return math.pi * (self.radius ** 2) 
